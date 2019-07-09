dc3a308312d6d957071cf279154f824a348f33cd
lucas moura <lucas123.moura@gmail.com>
2019-07-09 10:30:02 -0300

    ByteBank05
    Adicionando o construtor da classe e o atributo estático para realizar a contagem de contas.

---
+++ 05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,84 @@
+﻿namespace _05_ByteBank
+{
+	class ContaCorrente
+	{
+		private int _agencia;
+		public int Agencia
+		{
+			get
+			{
+				return _agencia;
+			}
+			set
+			{
+				if (value <= 0)
+				{
+					return;
+				}
+
+				_agencia = value;
+			}
+		}
+		public int Numero { get; set; }
+		private double _saldo;
+
+		public double Saldo
+		{
+			get
+			{
+				return _saldo;
+			}
+			set
+			{
+				if (value < 0)
+				{
+					return;
+				}
+
+				_saldo = value;
+			}
+		}
+
+
+
+		public Cliente Titular { get; set; }
+		public static int TotalDeContasCriadas { get; private set; }
+
+
+
+
+		public ContaCorrente(int agencia, int numero)
+		{
+			this.Agencia = agencia;
+			this.Numero = numero;
+			ContaCorrente.TotalDeContasCriadas++;
+		}
+
+		public void Depositar(double valor)
+		{
+			this.Saldo += valor;
+		}
+
+		public bool Sacar(double valor)
+		{
+			if (this.Saldo < valor)
+			{
+				return false;
+			}
+			this.Saldo -= valor;
+			return true;
+		}
+
+		public bool Transferir(ContaCorrente contaDestino, double valor)
+		{
+			if (this.Saldo < valor)
+			{
+				return false;
+			}
+			this.Saldo -= valor;
+
+			contaDestino.Depositar(valor);
+			return true;
+		}
+	}
+}
---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+			ContaCorrente primeiraContaCorrente = new ContaCorrente(846, 8467951);
+
+			primeiraContaCorrente.Titular = new Cliente();
+			primeiraContaCorrente.Titular.Nome = "gabriela";
+			primeiraContaCorrente.Titular.Cpf = "46256323823";
+			primeiraContaCorrente.Depositar(200.00);
+
+			Console.WriteLine(primeiraContaCorrente.Titular.Nome);
+			Console.WriteLine(primeiraContaCorrente.Saldo);
+			Console.WriteLine(primeiraContaCorrente.Numero);
+
+			Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+
+			ContaCorrente segundaContaCorrente = new ContaCorrente(846, 8467951);
+
+			segundaContaCorrente.Titular = new Cliente();
+			segundaContaCorrente.Titular.Nome = "Lucas";
+
+			Console.WriteLine(segundaContaCorrente.Titular.Nome);
+			Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+			Console.ReadLine();
+		}
+	}
+}
